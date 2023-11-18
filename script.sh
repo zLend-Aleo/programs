74a690909fb9b275261a24aa778486e6213a3f70
Zk <zklim95@hotmail.com>
2023-11-18 05:56:13 +0300

    chore: uncomment script

--- script.sh
+++ script.sh
@@ -15,19 +15,19 @@
 
 
 
-# echo "==========Send credits to Admin Address==========="
-# snarkos developer execute --private-key APrivateKey1zkpBjpEgLo4arVUkQmcLdKQMiAKGaHAQVVwmF8HQby8vdYs --query http://localhost:3030 \
-# credits.aleo transfer_public "aleo1fjmx66zrmfxx288udrw93re4x9xvxkuszwuegmf78prt3shc2yrscf6shw" "50000000u64" \
-# --broadcast http://localhost:3030/testnet3/transaction/broadcast
-# sleep 4
+echo "==========Send credits to Admin Address==========="
+snarkos developer execute --private-key APrivateKey1zkpBjpEgLo4arVUkQmcLdKQMiAKGaHAQVVwmF8HQby8vdYs --query http://localhost:3030 \
+credits.aleo transfer_public "aleo1fjmx66zrmfxx288udrw93re4x9xvxkuszwuegmf78prt3shc2yrscf6shw" "50000000u64" \
+--broadcast http://localhost:3030/testnet3/transaction/broadcast
+sleep 4
 
-# echo "==========Check Balance==========="
-# curl http://localhost:3030/testnet3/program/credits.aleo/mapping/account/aleo1fjmx66zrmfxx288udrw93re4x9xvxkuszwuegmf78prt3shc2yrscf6shw
-# echo -e "\n"
+echo "==========Check Balance==========="
+curl http://localhost:3030/testnet3/program/credits.aleo/mapping/account/aleo1fjmx66zrmfxx288udrw93re4x9xvxkuszwuegmf78prt3shc2yrscf6shw
+echo -e "\n"
 
-# echo "=============Deploying zkID_NFT============="
-# snarkos developer deploy --private-key APrivateKey1zkpDZLpPdRhc2xNgyhbgPB7LY2KCfk1Yakn1RVwtaAEQAqe --query http://localhost:3030 \
-# --priority-fee 600 zkid_nft_001.aleo --path ./zkid_nft_001/build/ --broadcast http://localhost:3030/testnet3/transaction/broadcast
+echo "=============Deploying zkID_NFT============="
+snarkos developer deploy --private-key APrivateKey1zkpDZLpPdRhc2xNgyhbgPB7LY2KCfk1Yakn1RVwtaAEQAqe --query http://localhost:3030 \
+--priority-fee 600 zkid_nft_001.aleo --path ./zkid_nft_001/build/ --broadcast http://localhost:3030/testnet3/transaction/broadcast
 
 echo "=============Deploying zkID_LP============="
 snarkos developer deploy --private-key APrivateKey1zkpDZLpPdRhc2xNgyhbgPB7LY2KCfk1Yakn1RVwtaAEQAqe --query http://localhost:3030 \
@@ -38,40 +38,40 @@ snarkos developer deploy --private-key APrivateKey1zkpDZLpPdRhc2xNgyhbgPB7LY2KCf
 --priority-fee 600 zkid_auction_001.aleo --path ./zkid_auction_001/build/ --broadcast http://localhost:3030/testnet3/transaction/broadcast
 sleep 4
 
-# echo "==========Check Deployments==========="
-# curl http://localhost:3030/testnet3/program/zkid_auction_001.aleo
-# echo -e "\n"
+echo "==========Check Deployments==========="
+curl http://localhost:3030/testnet3/program/zkid_auction_001.aleo
+echo -e "\n"
 
-# echo "==========Send credits to User Address==========="
-# snarkos developer execute --private-key APrivateKey1zkpBjpEgLo4arVUkQmcLdKQMiAKGaHAQVVwmF8HQby8vdYs --query http://localhost:3030 \
-# credits.aleo transfer_public "aleo1gf64vc4kxlpapnmfq72e0dshtshv43gezf7fnw555llm7mf5dqzsr6pf60" "50000000u64" \
-# --broadcast http://localhost:3030/testnet3/transaction/broadcast
-# sleep 4
+echo "==========Send credits to User Address==========="
+snarkos developer execute --private-key APrivateKey1zkpBjpEgLo4arVUkQmcLdKQMiAKGaHAQVVwmF8HQby8vdYs --query http://localhost:3030 \
+credits.aleo transfer_public "aleo1gf64vc4kxlpapnmfq72e0dshtshv43gezf7fnw555llm7mf5dqzsr6pf60" "50000000u64" \
+--broadcast http://localhost:3030/testnet3/transaction/broadcast
+sleep 4
 
-# echo "==========Check Balance==========="
-# curl http://localhost:3030/testnet3/program/credits.aleo/mapping/account/aleo1gf64vc4kxlpapnmfq72e0dshtshv43gezf7fnw555llm7mf5dqzsr6pf60
-# echo -e "\n"
+echo "==========Check Balance==========="
+curl http://localhost:3030/testnet3/program/credits.aleo/mapping/account/aleo1gf64vc4kxlpapnmfq72e0dshtshv43gezf7fnw555llm7mf5dqzsr6pf60
+echo -e "\n"
 
-# echo "==========Initialize zkID==========="
-# snarkos developer execute --private-key APrivateKey1zkpDZLpPdRhc2xNgyhbgPB7LY2KCfk1Yakn1RVwtaAEQAqe --query http://localhost:3030 \
-# zkid_nft_001.aleo initialize_collection "111u128" \
-# --broadcast http://localhost:3030/testnet3/transaction/broadcast
-# sleep 4
+echo "==========Initialize zkID==========="
+snarkos developer execute --private-key APrivateKey1zkpDZLpPdRhc2xNgyhbgPB7LY2KCfk1Yakn1RVwtaAEQAqe --query http://localhost:3030 \
+zkid_nft_001.aleo initialize_collection "111u128" \
+--broadcast http://localhost:3030/testnet3/transaction/broadcast
+sleep 4
 
-# echo "==========Issue zkID to User Address==========="
-# snarkos developer execute --private-key APrivateKey1zkpDZLpPdRhc2xNgyhbgPB7LY2KCfk1Yakn1RVwtaAEQAqe --query http://localhost:3030 \
-# zkid_nft_001.aleo issue_zkid "aleo12ux3gdauck0v60westgcpqj7v8rrcr3v346e4jtq04q7kkt22czsh808v2" "{data0: 0u128, data1: 1u128, data2: 2u128, data3: 3u128}" \
-# --broadcast http://localhost:3030/testnet3/transaction/broadcast
-# sleep 4
+echo "==========Issue zkID to User Address==========="
+snarkos developer execute --private-key APrivateKey1zkpDZLpPdRhc2xNgyhbgPB7LY2KCfk1Yakn1RVwtaAEQAqe --query http://localhost:3030 \
+zkid_nft_001.aleo issue_zkid "aleo12ux3gdauck0v60westgcpqj7v8rrcr3v346e4jtq04q7kkt22czsh808v2" "{data0: 0u128, data1: 1u128, data2: 2u128, data3: 3u128}" \
+--broadcast http://localhost:3030/testnet3/transaction/broadcast
+sleep 4
 
-# echo "==========Scan for zkID record==========="
-# snarkos developer scan --private-key APrivateKey1zkpBjpEgLo4arVUkQmcLdKQMiAKGaHAQVVwmF8HQby8vdYs --endpoint http://localhost:3030  --last 10
+echo "==========Scan for zkID record==========="
+snarkos developer scan --private-key APrivateKey1zkpBjpEgLo4arVUkQmcLdKQMiAKGaHAQVVwmF8HQby8vdYs --endpoint http://localhost:3030  --last 10
 
-# echo "==========Convert credits to private==========="
-# snarkos developer execute --private-key APrivateKey1zkpBjpEgLo4arVUkQmcLdKQMiAKGaHAQVVwmF8HQby8vdYs --query http://localhost:3030 \
-# credits.aleo transfer_public_to_private "aleo12ux3gdauck0v60westgcpqj7v8rrcr3v346e4jtq04q7kkt22czsh808v2" "10000000u64" \
-# --broadcast http://localhost:3030/testnet3/transaction/broadcast
-# sleep 4
+echo "==========Convert credits to private==========="
+snarkos developer execute --private-key APrivateKey1zkpBjpEgLo4arVUkQmcLdKQMiAKGaHAQVVwmF8HQby8vdYs --query http://localhost:3030 \
+credits.aleo transfer_public_to_private "aleo12ux3gdauck0v60westgcpqj7v8rrcr3v346e4jtq04q7kkt22czsh808v2" "10000000u64" \
+--broadcast http://localhost:3030/testnet3/transaction/broadcast
+sleep 4
 
 echo "==========Add liquidity==========="
 snarkos developer execute --private-key APrivateKey1zkpBjpEgLo4arVUkQmcLdKQMiAKGaHAQVVwmF8HQby8vdYs --query http://localhost:3030 \
